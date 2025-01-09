006032d041d28f9ed6a9878fc1de9f017c39bdc8
LamJoey <lamjoey@sheridancollege.ca>
2025-01-08 20:30:54 -0500

    added a second method to check the boundries

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -19,11 +19,14 @@ public class FirstScript : MonoBehaviour
         Vector2 pos = transform.position;
         pos.x += speed;
 
-        //what we test on right
+
+        //1st method/////////////////////////////////////////////////////////////
         Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+ 
+        //what we test on right
         Vector2 screenSizeInWorld = new Vector2();
         screenSizeInWorld = Camera.main.ScreenToWorldPoint(screenSize);
-
+   
         //what we test on left
         Vector2 screenZeroInWorld = Camera.main.ScreenToWorldPoint(Vector2.zero);
 
@@ -31,6 +34,17 @@ public class FirstScript : MonoBehaviour
         {
             speed *= -1;
         }
+        //////////////////////////////////////////////////////////////////////////
+   
+        /* 2nd method/////////////////////////////////////////////////////////////
+         * 
+         * transform the square pos in world to screen
+         * Vector2 squareScreenPos = Camera.main.WorldToScreenPoint(pos);
+         * 
+         * if(squareScreenPos.x < 0 || squareScreenPos.x > Screen.width){
+         *   speed *= -1;
+         * }
+         *////////////////////////////////////////////////////////////////////////
 
         transform.position = pos;
 
