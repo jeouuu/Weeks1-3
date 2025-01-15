f825d81f151e342152d205126d78ac4277cd8f1d
LamJoey <lamjoey@sheridancollege.ca>
2025-01-15 16:49:53 -0500

    CodingGym-BouncingBall: 4,5. Finished.

--- Assets/Scripts/BouncingBall.cs
+++ Assets/Scripts/BouncingBall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BouncingBall : MonoBehaviour
 {
     public Vector2 speed;
-    float direction = -1;
+    float direction = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,64 +16,77 @@ public class BouncingBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //get the circle to move 
+        //get the circle Position 
         Vector2 cirPos = transform.position;
-        cirPos.x += speed.x * Time.deltaTime;
-       // cirPos.y += speed.y * 2f * Time.deltaTime;
+
+        MoveBall(cirPos);
+        ChangeSize();
+        ChangeSpeed(); 
+        ResetBall(cirPos);
+    }
+
+    void MoveBall(Vector2 cirPos) {
+        //update the postion with speed
+        cirPos.x += speed.x * direction * Time.deltaTime;
+        cirPos.y += speed.y * direction * 2f * Time.deltaTime;
         transform.position = cirPos;
 
         //check boundaries 
         //transform the ball position from world to space
         Vector2 cirScreenPos = Camera.main.WorldToScreenPoint(cirPos);
-        if(cirScreenPos.x < 0)
-        {
+        if (cirScreenPos.x < 0) {
             Vector2 fixedPos = new Vector2(0, 0);
             cirPos.x = fixedPos.x;
-            cirPos.x = speed.x * direction * Time.deltaTime;
+            direction *= -1;
         }
-        if ( cirScreenPos.x > Screen.width )
-        {
+        if (cirScreenPos.x > Screen.width) {
             Vector2 fixedPos = new Vector2(Screen.width, 0);
             cirPos.x = fixedPos.x;
-            cirPos.x += speed.x * direction ;
-            //Debug.Log("touch");
+            direction *= -1;
         }
-        if (cirScreenPos.y < 0)
-        {
+        if (cirScreenPos.y < 0) {
             Vector2 fixedPos = new Vector2(0, 0);
             cirPos.y = fixedPos.y;
-            cirPos.y += speed.y * direction * Time.deltaTime;
+            direction *= -1;
         }
-        if (cirScreenPos.y > Screen.height)
-        {
+        if (cirScreenPos.y > Screen.height) {
             Vector2 fixedPos = new Vector2(0, Screen.height);
             cirPos.y = fixedPos.y;
-            cirPos.y += speed.y * direction * Time.deltaTime;
+            direction *= -1;
         }
+    }
 
-        //use up/down to change the size
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
+    void ChangeSize() {
+        //use up/down to change the size of the ball
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
             Vector2 cirSize = transform.localScale;
             cirSize.x += 0.5f;
             cirSize.y += 0.5f;
             transform.localScale = cirSize;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
             Vector2 cirSize = transform.localScale;
             cirSize.x -= 0.5f;
             cirSize.y -= 0.5f;
             transform.localScale = cirSize;
         }
+    }
 
-        //use left/right to change the speed
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            speed -= new Vector2(1, 1); 
+    void ChangeSpeed() {
+        //use left/right to change the speed of the ball
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            speed -= new Vector2(1, 1);
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            speed += new Vector2(1, 1);
+        }
+    }
 
-        Debug.Log(speed.x);
-
+    void ResetBall(Vector2 cirPos) {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            cirPos = new Vector2(0, 0);
+            speed = new Vector2(Random.Range(1f, 10f), Random.Range(1f, 10f));
+            transform.position = cirPos;
+        }
     }
 }
