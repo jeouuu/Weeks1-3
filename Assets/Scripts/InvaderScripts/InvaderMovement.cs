c743705f12fc238c4ffe3f8fe1cd41864253cfda
LamJoey <lamjoey@sheridancollege.ca>
2025-02-03 01:16:55 -0500

    Fixed week2 CodingGym - Invader.

--- Assets/Scripts/InvaderScripts/InvaderMovement.cs
+++ Assets/Scripts/InvaderScripts/InvaderMovement.cs
@@ -6,21 +6,57 @@ using static UnityEditor.PlayerSettings;
 
 public class InvaderMovement : MonoBehaviour
 {
-    public Vector2 speed = new Vector2(0f,-5f);
+    public Vector2 speed = new Vector2(5f,0f);
     public float offset = 5f;
+    bool reachEdge = false;
 
-    // Update is called once per frame
     void Update() {
-        //move the invader, march down
+
+        //move the invader, horizontally
         Vector2 pos = transform.position;
 
-        MoveInvader(pos);
+        Vector2 posInScreen = Camera.main.WorldToScreenPoint(pos);
+
+        if (posInScreen.x > Screen.width )
+        {
+            Vector2 fixedPos = new Vector2(Screen.width -1, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            transform.position = pos;
+            reachEdge = true;
+        } else if (posInScreen.x < 0)
+        {
+            Vector2 fixedPos = new Vector2(0 + 1, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            transform.position = pos;
+            reachEdge = true;
+        } else
+        {
+            reachEdge= false;
+        }
+
+        if (reachEdge) 
+        {
+            MoveInvaderVertical(pos);
+            speed.x *= -1;
+         
+        } else
+        {
+            MoveInvaderHorizontal(pos); 
+        }
+
         ReachGround(pos);
     }
 
-    private void MoveInvader(Vector2 pos) {
-        pos.y += speed.y * Time.deltaTime;
+    private void MoveInvaderHorizontal(Vector2 pos) {
+        pos.x += speed.x * Time.deltaTime;
         transform.position = pos;
+    }
+
+    private void MoveInvaderVertical(Vector2 pos)
+    {
+        pos.y = pos.y - 1;
+        transform.position = pos;
+        reachEdge = false;
     }
 
     private void ReachGround(Vector2 pos) {
